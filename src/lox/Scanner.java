42fb2e9dc0e4adadcf6e392fc8797ec40245dac9
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2023-02-05 00:38:24 +0200

    fixed a bug in the scanToken function

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -78,6 +78,7 @@ public class Scanner {
             case '.': addToken(DOT); break;
             case '-': addToken(MINUS); break;
             case '+': addToken(PLUS); break;
+            case '*': addToken(STAR); break;
             case ';': addToken(SEMICOLON); break;
             case '!':
                 addToken(match('=') ? BANG_EQUAL : BANG);
